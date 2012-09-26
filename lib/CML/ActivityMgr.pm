6820353f099fd9478e6a46bc8787ac33943baaed
Stephen j. Smith <sjs@khadrin.com>
2012-09-25 21:52:13 -0400

    fix for --email-to is ignored; version 1.0 candidate

--- lib/CML/ActivityMgr.pm
+++ lib/CML/ActivityMgr.pm
@@ -1,6 +1,6 @@
 package CML::ActivityMgr;
 use Moose;
-our $VERSION = '0.80';
+our $VERSION = '1.00';
 
 =head1 NAME
 
@@ -8,7 +8,7 @@ CML::ActivityMgr - Columbus Metro Library activity manager and auto-renewer.
 
 =head1 VERSION
 
-Version 0.80
+Version 1.00
 
 =cut
 
